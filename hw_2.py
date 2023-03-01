6ce0669c186c8d6e4e7dd619453e06e1428bacf1
msindkar <118861650+msindkar@users.noreply.github.com>
2023-03-01 00:28:15 -0500

    Update hw_2.py

--- hw_2.py
+++ hw_2.py
@@ -3,7 +3,7 @@
 #
 
 import numpy as np
-from hw_1_func import *
+from hw_1_func import supersonic_nozzle_exact_solution
 #import matplotlib as plt
 
 p0 = 3E5 # Stagnation pressure, Pa
@@ -18,9 +18,9 @@ imax = int(input())
 print('1 for isentropic case, anything else for shock case')
 if int(input()) == 1:
     shock_flag = 0
+    exact_solution = supersonic_nozzle_exact_solution(p0, t0, imax)
 else:
     shock_flag = 1
 
 center_array = np.zeros((1, imax + 2))                   # Array of cell centers, even number, 2 ghost cells
 interface_array = np.zeros((1, imax + 3))                # Array of interfaces, odd number, face at throat, 2 ghost cells
-
