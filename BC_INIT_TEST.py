86b8671bb5075980bcee5e0b8aa815d2ec28ccc2
msindkar <118861650+msindkar@users.noreply.github.com>
2023-05-26 16:43:20 +0530

    apply bc/init to centers rather than nodes

--- BC_INIT_TEST.py
+++ BC_INIT_TEST.py
@@ -1,4 +1,4 @@
-def load_BC_init(imax, jmax, x, y):    
+def load_BC_init(imax, jmax, x_cell, y_cell):    
     # BC/INIT CALC
     from config import supersonic
     import numpy as np
@@ -19,16 +19,16 @@ def load_BC_init(imax, jmax, x, y):
         print('E: Invalid value in supersonic flag, please check the config.py file')
         raise SystemExit(0)
     
-    prim = np.zeros((imax, jmax, 4)) # rho u v p
+    prim = np.zeros((imax - 1, jmax - 1, 4)) # rho u v p
     
-    prim[:, :, 0] = phi_rho[0] + phi_rho[1]*np.sin(phi_rho[3]*np.pi*x/L) + phi_rho[2]*np.cos(phi_rho[4]*np.pi*y/L)
-    prim[:, :, 1] = phi_u[0]   + phi_u[1]*np.sin(phi_u[3]*np.pi*x/L)     + phi_u[2]*np.cos(phi_u[4]*np.pi*y/L)
-    prim[:, :, 2] = phi_v[0]   + phi_v[1]*np.cos(phi_v[3]*np.pi*x/L)     + phi_v[2]*np.sin(phi_v[4]*np.pi*y/L)
-    prim[:, :, 3] = phi_p[0]   + phi_p[1]*np.cos(phi_p[3]*np.pi*x/L)     + phi_p[2]*np.sin(phi_p[4]*np.pi*y/L)
+    prim[:, :, 0] = phi_rho[0] + phi_rho[1]*np.sin(phi_rho[3]*np.pi*x_cell/L) + phi_rho[2]*np.cos(phi_rho[4]*np.pi*y_cell/L)
+    prim[:, :, 1] = phi_u[0]   + phi_u[1]*np.sin(phi_u[3]*np.pi*x_cell/L)     + phi_u[2]*np.cos(phi_u[4]*np.pi*y_cell/L)
+    prim[:, :, 2] = phi_v[0]   + phi_v[1]*np.cos(phi_v[3]*np.pi*x_cell/L)     + phi_v[2]*np.sin(phi_v[4]*np.pi*y_cell/L)
+    prim[:, :, 3] = phi_p[0]   + phi_p[1]*np.cos(phi_p[3]*np.pi*x_cell/L)     + phi_p[2]*np.sin(phi_p[4]*np.pi*y_cell/L)
     
     bot   = prim[:, 0, :]
     left  = prim[0, :, :]
-    up    = prim[:, jmax - 1, :]
-    right = prim[imax - 1, :, :]
+    up    = prim[:, jmax - 2, :]
+    right = prim[imax - 2, :, :]
     
     return prim, left, bot, right, up
