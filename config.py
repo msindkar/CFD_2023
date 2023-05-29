ea4154dd4b33eef98747cf61a58105b6288d2601
msindkar <118861650+msindkar@users.noreply.github.com>
2023-05-29 16:07:10 +0530

    added flux config

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 # CFD code input file
 
-gridname   = 'curv2d257.grd'
-supersonic = 0
+gridname    = 'curv2d257.grd'
+supersonic  = 0               # 0 for subsonic, 1 for supersonic 
+flux_scheme = 'vl1'           # 'vl1' for van Leer 1st order, 'vl2', 'roe1', 'roe2'
--- maintest.py
+++ maintest.py
@@ -6,3 +6,6 @@ from area_calc import area_normals_calc
 imax, jmax, x, y, x_cell, y_cell = mesh_loader()
 prim, BC_left, BC_bot, BC_right, BC_up = load_BC_init(imax, jmax, x_cell, y_cell)
 A_vert, A_hori, normals_h, normals_v = area_normals_calc(imax, jmax, x, y)
+
+def van_leer_flux():
+    ''
