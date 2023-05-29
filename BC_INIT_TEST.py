0c3b62a58c7d92645289a1a0366d24c443e316d7
msindkar <118861650+msindkar@users.noreply.github.com>
2023-05-29 16:14:01 +0530

    allocated mms phi's as persistent
    can't keep allocating and deallocating phi's every time src terms calc

--- BC_INIT_TEST.py
+++ BC_INIT_TEST.py
@@ -1,23 +1,8 @@
-def load_BC_init(imax, jmax, x_cell, y_cell):    
+def load_BC_init(phi_rho, phi_u, phi_v, phi_p, imax, jmax, x_cell, y_cell):    
     # BC/INIT CALC
-    from config import supersonic
     import numpy as np
     
     L = 1
-    
-    if supersonic == 1:
-        phi_rho = np.array([1.0, 0.15, -0.1, 1.0, 0.5]) # phi0 phix phiy aphix aphiy
-        phi_u   = np.array([800.0, 50.0, -30.0, 1.5, 0.6])
-        phi_v   = np.array([800.0, -75.0, 40.0, 0.5, 2/3])
-        phi_p   = np.array([1.0e5, 0.2e5, 0.5e5, 2, 1])
-    elif supersonic == 0:
-        phi_rho = np.array([1.0, 0.15, -0.1, 1.0, 0.5]) # phi0 phix phiy aphix aphiy
-        phi_u   = np.array([70.0, 5.0, -7.0, 1.5, 0.6])
-        phi_v   = np.array([90.0, -15.0, 8.5, 0.5, 2/3])
-        phi_p   = np.array([1.0e5, 0.2e5, 0.5e5, 2, 1])
-    else:
-        print('E: Invalid value in supersonic flag, please check the config.py file')
-        raise SystemExit(0)
     
     prim = np.zeros((imax - 1, jmax - 1, 4)) # rho u v p
     
--- src_mms.py
+++ src_mms.py
@@ -1,8 +1,8 @@
+from config import supersonic
 import numpy as np
 from mesh_loader import mesh_loader
-from BC_INIT_TEST import load_BC_init
-from area_calc import area_normals_calc
-from config import supersonic
+
+L = 1
 
 if supersonic == 1:
     phi_rho = np.array([1.0, 0.15, -0.1, 1.0, 0.5]) # phi0 phix phiy aphix aphiy
@@ -17,10 +17,5 @@ elif supersonic == 0:
 else:
     print('E: Invalid value in supersonic flag, please check the config.py file')
     raise SystemExit(0)
-
+    
 imax, jmax, x, y, x_cell, y_cell = mesh_loader()
-prim, BC_left, BC_bot, BC_right, BC_up = load_BC_init(phi_rho, phi_u, phi_v, phi_p, imax, jmax, x_cell, y_cell)
-A_vert, A_hori, normals_h, normals_v = area_normals_calc(imax, jmax, x, y)
-
-def van_leer_flux():
-    ''
